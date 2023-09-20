c3063076a85efe105ba984b8841ea7db28a92ea2
Kevin Rodriguez <kyrodriguez97@gmail.com>
2023-09-19 22:05:55 -0500

    diff --git a/pages/about.vue b/pages/about.vue new file mode 100644 index 0000000..67e4bb8 --- /dev/null +++ b/pages/about.vue @@ -0,0 +1,11 @@ +<template> +  <b-container class="main h-100" fluid> +    <navbar /> +    <b-container> +      <b-row class="justify-content-center align-items-center"> +        <b-col sm="12" md="6">1 of 2</b-col> +        <b-col sm="12" md="6">2 of 2</b-col> +      </b-row> +    </b-container> +  </b-container> +</template> \ No newline at end of file

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-container class="main h-100" fluid>
+    <navbar />
+    <b-container>
+      <b-row class="justify-content-center align-items-center">
+        <b-col sm="12" md="6">1 of 2</b-col>
+        <b-col sm="12" md="6">2 of 2</b-col>
+      </b-row>
+    </b-container>
+  </b-container>
+</template>
