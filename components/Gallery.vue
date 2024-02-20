a3d3d85d6ca4d57f8f2109020ba0b91211fed715
Kevin Rodriguez <kyrodriguez97@gmail.com>
2024-02-20 13:28:09 -0500

    Change gallery load

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -3,10 +3,10 @@
     <stack :monitor-images-loaded="true" :column-min-width="450" :gutter-width="15" :gutter-height="15">
       <stack-item v-for="image of images" :key="image.slug">
         <nuxt-img v-b-modal="image.slug" preload :src="image.photograph" :alt="`${image.title} by thekevshot`"
-          class="gallery-image" loading="lazy" sizes="sm:450px md:576px xl:450px" quality="90" format="webp" />
+          class="gallery-image" sizes="sm:450px md:576px xl:450px" quality="80" />
         <b-modal :id="image.slug" size="xl" hide-footer>
           <nuxt-img class="modal-image" v-b-modal="image.slug" preload :src="image.photograph"
-            :alt="`${image.title} by thekevshot`" loading="lazy" quality="90" format="webp" placeholder />
+            :alt="`${image.title} by thekevshot`" sizes="sm:720px md:1080px" loading="lazy" quality="90" placeholder />
         </b-modal>
       </stack-item>
     </stack>
