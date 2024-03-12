55585f37809bfeb0e3848528cef61314a7c6a9ba
Kevin Rodriguez <kyrodriguez97@gmail.com>
2024-03-12 17:39:41 -0500

    Remove async scripts rendering

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -2,7 +2,7 @@
   <div id="gallery">
     <stack :monitor-images-loaded="true" :column-min-width="450" :gutter-width="15" :gutter-height="15">
       <stack-item style="transition: transform 300ms" v-for="image of images" :key="image.slug">
-        <nuxt-img v-b-modal="image.slug" preload :src="image.photograph" :alt="`${image.title} by thekevshot`"
+        <nuxt-img v-b-modal="image.slug" placeholder preload :src="image.photograph" :alt="`${image.title} by thekevshot`"
           class="gallery-image" sizes="sm:450px md:576px xl:450px" quality="80" />
         <b-modal :id="image.slug" size="xl" hide-footer>
           <nuxt-img class="modal-image" v-b-modal="image.slug" preload :src="image.photograph"
