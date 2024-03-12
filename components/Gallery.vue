72712a76b3619d7e516cd1277b6b7021c3b7cd4b
Kevin Rodriguez <kyrodriguez97@gmail.com>
2024-03-12 18:49:01 -0500

    Add explict width and height for images

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -2,8 +2,9 @@
   <div id="gallery">
     <stack :monitor-images-loaded="true" :column-min-width="450" :gutter-width="15" :gutter-height="15">
       <stack-item style="transition: transform 300ms" v-for="image of images" :key="image.slug">
-        <nuxt-img v-b-modal="image.slug" placeholder preload :src="image.photograph" :alt="`${image.title} by thekevshot`"
-          class="gallery-image" sizes="sm:450px md:576px xl:450px" quality="80" />
+        <nuxt-img
+        v-b-modal="image.slug" placeholder preload :src="image.photograph" width="450" height="300"
+          :alt="`${image.title} by thekevshot`" class="gallery-image" sizes="sm:450px md:576px xl:450px" quality="80" />
         <b-modal :id="image.slug" size="xl" hide-footer>
           <nuxt-img class="modal-image" v-b-modal="image.slug" preload :src="image.photograph"
             :alt="`${image.title} by thekevshot`" sizes="sm:720px md:1080px" loading="lazy" quality="90" placeholder />
