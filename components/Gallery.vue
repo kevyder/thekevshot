97823824909887688cbc95622e50d6498e2f5dd8
Kevin Rodriguez <kyrodriguez97@gmail.com>
2024-03-12 20:23:49 -0500

    Change images size

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -3,7 +3,7 @@
     <stack :monitor-images-loaded="true" :column-min-width="450" :gutter-width="15" :gutter-height="15">
       <stack-item style="transition: transform 300ms" v-for="image of images" :key="image.slug">
         <nuxt-img v-b-modal="image.slug" format="webp" placeholder preload :src="image.photograph" width="450" height="300"
-          :alt="`${image.title} by thekevshot`" class="gallery-image" sizes="sm:450px md:576px xl:720px" quality="90" />
+          :alt="`${image.title} by thekevshot`" class="gallery-image" sizes="sm:450px md:450px xl:450px"/>
         <b-modal :id="image.slug" size="xl" hide-footer>
           <nuxt-img class="modal-image" v-b-modal="image.slug" format="webp" width="1080" height="720" :src="image.photograph"
             :alt="`${image.title} by thekevshot`" sizes="sm:720px md:1080px" loading="lazy" quality="90" placeholder />
