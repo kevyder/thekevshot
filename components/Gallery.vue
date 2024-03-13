e56fe23a95f8fdd8739f76f6a06d25cdc1b48266
Kevin Rodriguez <kyrodriguez97@gmail.com>
2024-03-12 20:06:49 -0500

    Changes images quality

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -2,11 +2,10 @@
   <div id="gallery">
     <stack :monitor-images-loaded="true" :column-min-width="450" :gutter-width="15" :gutter-height="15">
       <stack-item style="transition: transform 300ms" v-for="image of images" :key="image.slug">
-        <nuxt-img
-        v-b-modal="image.slug" placeholder preload :src="image.photograph" width="450" height="300"
-          :alt="`${image.title} by thekevshot`" class="gallery-image" sizes="sm:450px md:576px xl:450px" quality="80" />
+        <nuxt-img v-b-modal="image.slug" format="webp" placeholder preload :src="image.photograph" width="450" height="300"
+          :alt="`${image.title} by thekevshot`" class="gallery-image" sizes="sm:450px md:576px xl:720px" quality="90" />
         <b-modal :id="image.slug" size="xl" hide-footer>
-          <nuxt-img class="modal-image" v-b-modal="image.slug" preload :src="image.photograph"
+          <nuxt-img class="modal-image" v-b-modal="image.slug" format="webp" width="1080" height="720" :src="image.photograph"
             :alt="`${image.title} by thekevshot`" sizes="sm:720px md:1080px" loading="lazy" quality="90" placeholder />
         </b-modal>
       </stack-item>
@@ -20,14 +19,15 @@
   width: 75%;
 }
 
-#gallery .gallery-image{
+#gallery .gallery-image {
   max-width: 100%;
   height: auto;
 }
 
 #gallery .gallery-image,
 .modal-image {
-  width: 100%;
+  max-width: 100%;
+  height: auto;
 }
 </style>
 
