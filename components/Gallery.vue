6c88e60335e7bf6d7f057b0bdcaf909a7b574ad6
Kevin Rodríguez ✌️ <kyrodriguez97@gmail.com>
2024-05-06 21:18:45 -0500

    Change gallery images quality

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -2,11 +2,11 @@
   <div id="gallery">
     <stack :monitor-images-loaded="true" :column-min-width="450" :gutter-width="15" :gutter-height="15">
       <stack-item style="transition: transform 300ms" v-for="image of images" :key="image.slug">
-        <nuxt-img v-b-modal="image.slug" format="webp" placeholder preload :src="image.photograph" width="450" height="300"
+        <nuxt-img v-b-modal="image.slug" placeholder preload :src="image.photograph" width="450" height="300"
           :alt="`${image.title} by thekevshot`" class="gallery-image" sizes="sm:450px md:450px xl:450px"/>
         <b-modal :id="image.slug" size="xl" hide-footer>
-          <nuxt-img class="modal-image" v-b-modal="image.slug" format="webp" width="1080" height="720" :src="image.photograph"
-            :alt="`${image.title} by thekevshot`" sizes="sm:720px md:1080px" loading="lazy" quality="90" placeholder />
+          <nuxt-img class="modal-image" v-b-modal="image.slug" width="1620" height="1080" :src="image.photograph"
+            :alt="`${image.title} by thekevshot`" sizes="sm:1080px md:1620px" loading="lazy" quality="90" placeholder />
         </b-modal>
       </stack-item>
     </stack>
