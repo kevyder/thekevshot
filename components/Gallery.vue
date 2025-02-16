9d6adac49701c7bd77ee56fef88cbe46587cfaf5
Kevin Rodríguez ✌️ <kyrodriguez97@gmail.com>
2025-02-16 14:44:54 -0500

    feat: sets images quality in env vars

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -3,10 +3,10 @@
     <stack :monitor-images-loaded="true" :column-min-width="450" :gutter-width="15" :gutter-height="15">
       <stack-item style="transition: transform 300ms" v-for="image of images" :key="image.slug">
         <nuxt-img v-b-modal="image.slug" placeholder preload :src="image.photograph" width="450" height="300"
-          :alt="`${image.title} by thekevshot`" class="gallery-image" sizes="sm:450px md:450px xl:450px"/>
+          :alt="`${image.title} by thekevshot`" class="gallery-image" :quality="`${gridImageQuality}`" sizes="sm:450px md:450px xl:450px"/>
         <b-modal :id="image.slug" size="xl" hide-footer>
           <nuxt-img class="modal-image" v-b-modal="image.slug" width="1620" height="1080" :src="image.photograph"
-            :alt="`${image.title} by thekevshot`" sizes="sm:1080px md:1620px" loading="lazy" quality="90" placeholder />
+            :alt="`${image.title} by thekevshot`" sizes="sm:1080px md:1620px" loading="lazy" :quality="`${modalImageQuality}`" placeholder />
         </b-modal>
       </stack-item>
     </stack>
@@ -36,6 +36,12 @@ export default {
   props: {
     images: {
       type: Array
+    }
+  },
+  data() {
+    return {
+      gridImageQuality: process.env.VUE_APP_GRID_IMAGE_QUALITY,
+      modalImageQuality: process.env.VUE_APP_MODAL_IMAGE_QUALITY
     }
   }
 }
