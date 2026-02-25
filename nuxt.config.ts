fd8827d85b085e01e80c9ddbe766b95fd70ff0ab
Kevin Rodríguez <kyrodriguez97@gmail.com>
2026-02-24 23:28:23 -0500

    fix: update 'resend' dependency version to remove caret fix: update gtag enabled condition to use NUXT_ENV

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
   },
 
   gtag: {
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: process.env.NUXT_ENV === 'production',
     id: process.env.NUXT_GTAG_ID,
   },
    turnstile: {
